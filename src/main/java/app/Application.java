4e0731117748db12593e3ac5fd2d1b48c87cbde6
Arima <kim111104@yandex.com>
2026-03-16 21:15:38 +0900

    Исправлены ошибки

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -8,13 +8,13 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Application {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
-                new AnnotationConfigApplicationContext(AppConfig.class);
-        for (int i = 0; i < 5; i++) {
-            AnimalsCage bean =
-                    applicationContext.getBean(AnimalsCage.class);
-            bean.whatAnimalSay();
+        try (AnnotationConfigApplicationContext applicationContext =
+                     new AnnotationConfigApplicationContext(AppConfig.class)) {
+            for (int i = 0; i < 5; i++) {
+                AnimalsCage bean =
+                        applicationContext.getBean(AnimalsCage.class);
+                bean.whatAnimalSay();
+            }
         }
     }
-
 }
